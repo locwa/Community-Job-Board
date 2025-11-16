449680d1384fe6bbed12c432fb178ee0c43cfa2a
locwa <94048557+locwa@users.noreply.github.com.>
2025-11-16 19:55:39 +0800

    added components and basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { JobList } from './job-list/job-list';
+import { CreateJob } from './create-job/create-job';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: JobList},
+  {path: "job-list", component: JobList},
+  {path: "create-job", component: CreateJob}
+];
---
+++ src/app/create-job/create-job.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-job',
+  imports: [],
+  templateUrl: './create-job.html',
+  styleUrl: './create-job.css',
+})
+export class CreateJob {
+
+}
