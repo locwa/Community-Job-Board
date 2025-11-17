7b451dd82c7cb9f7a8e209b8b4334720e3cfb9d6
locwa <94048557+locwa@users.noreply.github.com.>
2025-11-18 07:15:22 +0800

    added service and find jobs code

--- src/app/job-list/job-list.ts
+++ src/app/job-list/job-list.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { JobsService } from '../services/jobs-service';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-job-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './job-list.html',
   styleUrl: './job-list.css',
 })
 export class JobList {
+  jobs:any = []
+  jobTitleSearch = ""
+  jobCompanySearch = ""
+  jobLocationSearch = ""
+  jobTypeSearch= ""
 
+  filters:any = {
+    title: "",
+    company: "",
+    location: "",
+    type: ""
+  }
+
+  constructor(private jobService: JobsService) {}
+
+  ngOnInit() {
+    console.log(this.jobService.list());
+    this.jobs = this.jobService.list()
+  }
+
+  searchJob(){
+    this.filters = {
+      title: this.jobTitleSearch,
+      company: this.jobCompanySearch,
+      location: this.jobLocationSearch,
+      type: this.jobTypeSearch
+    }
+    this.jobs = this.jobService.list(this.filters)
+    console.log(this.jobs)
+  }
 }
