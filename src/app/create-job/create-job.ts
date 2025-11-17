b22f33f8729ce9a06501683091df539e37bfd0dc
locwa <94048557+locwa@users.noreply.github.com.>
2025-11-18 07:57:58 +0800

    added code for create job

--- src/app/create-job/create-job.ts
+++ src/app/create-job/create-job.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { JobsService } from '../services/jobs-service';
 
 @Component({
   selector: 'app-create-job',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './create-job.html',
   styleUrl: './create-job.css',
 })
 export class CreateJob {
+  title: string = "";
+  company: string = "";
+  location: string= "";
+  type: string = "";
+  description: string = "";
+  salary: string = "";
 
+  jobDetails:any = []
+
+  constructor(private jobService : JobsService) {
+  }
+
+  addJob(){
+    this.jobDetails = {
+      title: this.title,
+      company: this.company,
+      location: this.location,
+      type: this.type,
+      description: this.description,
+      salary: this.salary,
+    }
+
+    this.jobService.create(this.jobDetails)
+  }
 }
